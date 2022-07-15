a4e16c319356f40546489b425082142838d0e074
AthiraGreejith <athirasri1720@gmail.com>
2022-07-15 16:04:53 +0530

    added custom exceptions

--- lib/api_handler.dart
+++ lib/api_handler.dart
@@ -15,52 +15,74 @@ class ApiHandler {
 
   Future<dynamic> get(
       {required String url, HeaderOptions? headerOptions}) async {
-    Response<dynamic> response = await getDioIns(headerOptions).get(url);
-    dynamic responseJson = _response(response);
-    return responseJson;
+    try {
+      Response<dynamic> response = await getDioIns(headerOptions).get(url);
+      dynamic responseJson = _response(response);
+      return responseJson;
+    } on DioError catch (err) {
+      final errorMessage = CustomException.fromDioError(err).toString();
+      throw errorMessage;
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   Future<dynamic> post(
       {required String url,
       Map<String, dynamic>? body,
       HeaderOptions? headerOptions}) async {
-    Response<dynamic> response =
-        await getDioIns(headerOptions).post(url, data: body);
-    dynamic responseJson = _response(response);
-    return responseJson;
+    try {
+      Response<dynamic> response =
+          await getDioIns(headerOptions).post(url, data: body);
+      dynamic responseJson = _response(response);
+      return responseJson;
+    } on DioError catch (err) {
+      final errorMessage = CustomException.fromDioError(err).toString();
+      throw errorMessage;
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   Future<dynamic> delete(
       {required String url, HeaderOptions? headerOptions}) async {
-    Response<dynamic> response = await getDioIns(headerOptions).delete(url);
-    dynamic responseJson = _response(response);
-    return responseJson;
+    try {
+      Response<dynamic> response = await getDioIns(headerOptions).delete(url);
+      dynamic responseJson = _response(response);
+      return responseJson;
+    } on DioError catch (err) {
+      final errorMessage = CustomException.fromDioError(err).toString();
+      throw errorMessage;
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   Future<dynamic> put(
       {required String url,
       Map<String, dynamic>? body,
       HeaderOptions? headerOptions}) async {
-    Response<dynamic> response =
-        await getDioIns(headerOptions).put(url, data: body);
-    dynamic responseJson = _response(response);
-    return responseJson;
+    try {
+      Response<dynamic> response =
+          await getDioIns(headerOptions).put(url, data: body);
+      dynamic responseJson = _response(response);
+      return responseJson;
+    } on DioError catch (err) {
+      final errorMessage = CustomException.fromDioError(err).toString();
+      throw errorMessage;
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   dynamic _response(Response<dynamic> response) {
-    print(response.statusCode);
     switch (response.statusCode) {
       case 200:
         return response.data;
       case 202:
         return response.data;
-      case 400:
-        throw BadRequestException(response.data.toString());
-      case 403:
-        throw UnauthorisedException(response.data.toString());
       default:
-        throw FetchDataException(
-            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+        throw 'Error occured while Communication with Server with StatusCode : ${response.statusCode}';
     }
   }
 }
--- lib/custom_exceptions.dart
+++ lib/custom_exceptions.dart
@@ -1,33 +1,63 @@
-import 'api_errors.dart';
+import 'package:dio/dio.dart';
 
 class CustomException implements Exception {
-  final dynamic _message;
-  final String? _prefix;
+  late String errorMessage;
 
-  CustomException([this._message, this._prefix]);
+  CustomException.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioErrorType.cancel:
+        errorMessage = 'Request to the server was cancelled.';
+        break;
+      case DioErrorType.connectTimeout:
+        errorMessage = 'Connection timed out.';
+        break;
+      case DioErrorType.receiveTimeout:
+        errorMessage = 'Receiving timeout occurred.';
+        break;
+      case DioErrorType.sendTimeout:
+        errorMessage = 'Request send timeout.';
+        break;
+      case DioErrorType.response:
+        errorMessage = _handleStatusCode(dioError.response?.statusCode);
+        break;
+      case DioErrorType.other:
+        if (dioError.message.contains('SocketException')) {
+          errorMessage = 'No Internet.';
+          break;
+        }
+        errorMessage = 'Unexpected error occurred.';
+        break;
+      default:
+        errorMessage = 'Something went wrong';
+        break;
+    }
+  }
+
+  String _handleStatusCode(int? statusCode) {
+    switch (statusCode) {
+      case 400:
+        return 'Bad request.';
+      case 401:
+        return 'Authentication failed.';
+      case 403:
+        return 'The authenticated user is not allowed to access the specified API endpoint.';
+      case 404:
+        return 'The requested resource does not exist.';
+      case 405:
+        return 'Method not allowed. Please check the Allow header for the allowed HTTP methods.';
+      case 415:
+        return 'Unsupported media type. The requested content type or version number is invalid.';
+      case 422:
+        return 'Data validation failed.';
+      case 429:
+        return 'Too many requests.';
+      case 500:
+        return 'Internal server error.';
+      default:
+        return 'Oops something went wrong!';
+    }
+  }
 
   @override
-  String toString() {
-    return "$_prefix$_message";
-  }
-}
-
-class FetchDataException extends CustomException {
-  FetchDataException([String? message])
-      : super(message, ApiErrors.communicationError);
-}
-
-class BadRequestException extends CustomException {
-  BadRequestException([String? message])
-      : super(message, ApiErrors.invalidRequest);
-}
-
-class UnauthorisedException extends CustomException {
-  UnauthorisedException([String? message])
-      : super(message, ApiErrors.unauthorised);
-}
-
-class InvalidInputException extends CustomException {
-  InvalidInputException([String? message])
-      : super(message, ApiErrors.invalidInput);
+  String toString() => errorMessage;
 }
