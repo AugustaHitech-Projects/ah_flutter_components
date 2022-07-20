b0c250106d3b189ed62241fa1c2bce414aa107a6
AthiraGreejith <athirasri1720@gmail.com>
2022-07-20 10:29:29 +0530

    inline comments

--- lib/header_options.dart
+++ lib/header_options.dart
@@ -1,20 +1,21 @@
 import 'api_constants.dart';
 
 class HeaderOptions {
-  // Based on the bool, 'Authorization' will be added to the header in the format 'Bearer token_value'.
-  // Token will be taken from the shared preference saved with the keyword 'KEY_TOKEN'.
-  // The default value is 'true'
+  /// Based on the bool, 'Authorization' will be added to the header.
+  /// The default value is 'true'. 'Authorization' will be added in the format 'Bearer token_value'.
+  /// Token will be taken from the shared preference saved with the keyword 'KEY_TOKEN'.
   bool requireToken;
 
-  // The request Content-Type. The default value is 'application/json'.
-  // If empty string is passed, then default value will be taken.
+  /// The request Content-Type. The default value is 'application/json'.
+  /// If empty string is passed, then default value will be taken.
   String contentType;
 
-  // Http request additional headers need to append other than 'Authorization' and 'Content-Type'
+  /// Http request additional headers need to append other than 'Authorization' and 'Content-Type'
   Map<String, dynamic>? additionalHeaders;
 
-  HeaderOptions(
-      {this.requireToken = true,
-      this.contentType = ApiConstant.contentType,
-      this.additionalHeaders});
+  HeaderOptions({
+    this.requireToken = true,
+    this.contentType = ApiConstant.contentType,
+    this.additionalHeaders,
+  });
 }
