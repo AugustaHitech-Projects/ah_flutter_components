51b30ddb61e991c1be3a0aa3acfc991508d512c4
AthiraGreejith <athirasri1720@gmail.com>
2022-07-20 15:20:33 +0530

    custom exception code clean up

--- lib/api_handler.dart
+++ lib/api_handler.dart
@@ -2,6 +2,7 @@ library api_handler;
 
 import 'dart:async';
 import 'package:dio/dio.dart';
+import 'api_errors.dart';
 import 'api_interceptor.dart';
 import 'custom_exceptions.dart';
 import 'header_options.dart';
@@ -76,31 +77,10 @@ class ApiHandler {
   }
 
   dynamic _response(Response<dynamic> response) {
-    switch (response.statusCode) {
-      case 200:
-        return response.data;
-      case 202:
-        return response.data;
-      case 400:
-        return 'Bad request.';
-      case 401:
-        return 'Authentication failed.';
-      case 403:
-        return 'The authenticated user is not allowed to access the specified API endpoint.';
-      case 404:
-        return 'The requested resource does not exist.';
-      case 405:
-        return 'Method not allowed. Please check the Allow header for the allowed HTTP methods.';
-      case 415:
-        return 'Unsupported media type. The requested content type or version number is invalid.';
-      case 422:
-        return 'Data validation failed.';
-      case 429:
-        return 'Too many requests.';
-      case 500:
-        return 'Internal server error.';
-      default:
-        throw 'Error occured while Communication with Server with StatusCode : ${response.statusCode}';
+    if (response.statusCode == 200 || response.statusCode == 202) {
+      return response.data;
+    } else {
+      throw ApiErrors.getErrorBasedOnStatusCode(response.statusCode);
     }
   }
 }
